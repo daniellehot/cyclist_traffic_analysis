70591068910564f5080c951e824fc6f3af4d6cba
Daniel <dale@create.aau.dk>
2024-02-14 16:49:25 +0100

    ADD bytetracker utils

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,6 +2,7 @@ USER=daniel
 CODE_PRETRAINED=/home/daniel/cyclist_traffic_analysis/pretrained
 CODE_DATASETS=/home/daniel/cyclist_traffic_analysis/datasets
 CODE_YOLOX_OUTPUTS=/home/daniel/cyclist_traffic_analysis/YOLOX_outputs
+CODE_UTILS=/home/daniel/cyclist_traffic_analysis/bytetracker_utils
 #[ -z "$1" ] || CODE_CONTAINER=$1 # replace CODE_CONTAINER in case there is any 
 IMAGE=bytetrack
 
@@ -12,6 +13,7 @@ docker run --gpus all -it --rm \
     -v $CODE_PRETRAINED:/workspace/ByteTrack/pretrained \
     -v $CODE_DATASETS:/workspace/ByteTrack/datasets \
     -v $CODE_YOLOX_OUTPUTS:/workspace/ByteTrack/YOLOX_outputs \
+    -v $CODE_UTILS:/workspace/ByteTrack/bytetracker_utils \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -e XAUTHORITY=$XAUTH \
